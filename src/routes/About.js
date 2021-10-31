5ea353cad3a0b0543442f8f3728977651abd839e
LimChangHyun <stablestaple92@gmail.com>
2021-10-31 17:36:47 +0900

    1.0
    Basic React.js movie app with Nomad Coders

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About(props){
+    return (
+    <div className="about__container">
+        <span>
+            "Freedom is the freedom to say that two plus two make four. If that is granted, all else follows."
+        </span>
+        <span>- Goerge Orwell, 1984</span>  
+    </div>
+    );
+}
+
+export default About;
